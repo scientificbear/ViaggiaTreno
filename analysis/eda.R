25522b3036a6ecd87efdf1b13dc6fd0690a48e4c
Patrick <scientific.bear@gmail.com>
2019-03-14 22:02:25 +0100

    Progress

--- analysis/eda.R
+++ analysis/eda.R
@@ -7,12 +7,21 @@ library(tidyr)
 library(tibble)
 library(tidygraph)
 library(ggraph)
-library(extrafont)
+# library(extrafont)
 library(readr)
 library(forcats)
+library(stringr)
+library(leaflet)
+
 
 stations <- read_csv("../data/stations.csv") %>%
   rowid_to_column("id_number")
+
+stations %>% 
+  ggplot(aes(lon,lat)) +
+  geom_point(size=.3) + 
+  coord_fixed() + 
+  theme_minimal()
 
 df <- list.files(path="../data/train_status", pattern="*.csv") %>% 
   paste0("../data/train_status/", .) %>% 
@@ -77,16 +86,16 @@ df %>%
 setwd("~/Documents/repos/ViaggiaTreno/analysis")
 
 
-# fdate <- "2019-02-22"
+# fdate <- "2019-02-24"
 # raw_data <- list.files(path="../data/single_train_status", pattern=paste0(fdate,".csv")) %>%
 #   paste0("../data/single_train_status/", .) %>%
 #   lapply(., function(x) read.csv(x, stringsAsFactors = FALSE)) %>%
 #   do.call(rbind, .) %>%
 #   mutate(trip_date = as.Date(trip_date),
-#          from_planned_ts = as_datetime(from_planned, tz="Europe/Rome"),
-#          from_real_ts = as_datetime(from_real, tz="Europe/Rome"),
-#          to_planned_ts = as_datetime(to_planned, tz="Europe/Rome"),
-#          to_real_ts = as_datetime(to_real, tz="Europe/Rome"),
+#          from_planned_ts = as_datetime(from_planned-60*60*24*7*2, tz="Europe/Rome"),
+#          from_real_ts = as_datetime(from_real-60*60*24*7*2, tz="Europe/Rome"),
+#          to_planned_ts = as_datetime(to_planned-60*60*24*7*2, tz="Europe/Rome"),
+#          to_real_ts = as_datetime(to_real-60*60*24*7*2, tz="Europe/Rome"),
 #          inc_delay = as.numeric(inc_delay),
 #          seg_delay = as.numeric(seg_delay),
 #          fin_delay = as.numeric(fin_delay))
@@ -138,7 +147,9 @@ raw_data %>% summary()
 
 raw_data %>% 
   ggplot(aes(x=from_planned)) +
-geom_histogram(bins = ndays*6)
+  geom_histogram(bins = ndays*6) +
+  theme_minimal() + 
+  ggtitle("Number of observations by time")
 
 
 raw_data %>% 
@@ -151,9 +162,9 @@ raw_data %>%
   mutate(nomeLungo = ifelse(is.na(nomeLungo),from_id,nomeLungo)) %>% 
   select(nomeLungo, count) %>% 
   ggplot() +
-  geom_bar(aes(x=fct_reorder(nomeLungo,count), y=count), stat = "identity") +
+  geom_bar(aes(x=fct_reorder(nomeLungo,count), y=count/ndays), stat = "identity") +
   xlab("") +
-  ylab("") +
+  ylab("Number of trains (daily)") +
   ggtitle("Number of trains by station (top 20)") +
   coord_flip() +
   theme_minimal() +
@@ -173,37 +184,198 @@ raw_data %>%
   theme_minimal() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-raw_data %>%
-  filter(!is.na(seg_delay), abs(seg_delay/60)<0.5) %>% 
-  ggplot() +
-  geom_boxplot(aes(x="", y=seg_delay))
+raw_data %<>% 
+  filter(!is.na(seg_delay), abs(seg_delay)<100, abs(fin_delay)<180)
 
-raw_data %>%
-  filter(!is.na(seg_delay), abs(seg_delay)<30) %>% 
-  ggplot() +
-  geom_density(aes(seg_delay), bw=0.3, fill="grey", alpha=.6) +
-  geom_vline(xintercept = 0) + 
+temp <- raw_data %>%
+  filter(!is.na(seg_delay), abs(seg_delay)<100, abs(fin_delay)<180) %>% 
+  mutate(perc_inc_delay = percent_rank(inc_delay),
+         perc_seg_delay = percent_rank(seg_delay),
+         perc_fin_delay = percent_rank(fin_delay)) %>% 
+  select(inc_delay, perc_inc_delay, seg_delay, perc_seg_delay, fin_delay, perc_fin_delay) %>% 
+  gather("key_perc", "value_perc", -c("inc_delay", "seg_delay", "fin_delay")) %>% 
+  gather("key", "value", -c("key_perc", "value_perc")) %>% 
+  mutate(key_perc = str_replace_all(key_perc, "^perc_", "")) %>% 
+  filter(key_perc == key) %>% 
+  mutate(key = fct_relevel(key, "inc_delay", "seg_delay", "fin_delay")) %>% 
+  mutate(a = row_number()) %>% filter(a %% 100 == 0)
+
+max_min <- 60
+min_min <- -30
+
+(
+  temp %>% 
+    ggplot() +
+    geom_density(aes(value, fill=key, color=key), alpha=.4) +
+    geom_vline(xintercept = 0) + 
+    scale_color_brewer(palette="Set2") +
+    scale_fill_brewer(palette="Set2") +
+    ggtitle("Overall train punctuality") +
+    xlab("minutes") +
+    scale_x_continuous(limits = c(min_min, max_min)) +
+    theme_minimal() +
+    theme(legend.position = "none")
+) + (
+  temp %>% 
+    ggplot() +
+    geom_boxplot(aes(y=value, x=key, color=key)) +
+    geom_hline(yintercept = 0) +
+    scale_color_brewer(palette="Set2") +
+    scale_fill_brewer(palette="Set2") +
+    ylab("minutes") +
+    xlab("") +
+    scale_y_continuous(limits = c(min_min, max_min)) +
+    coord_flip() +
+    theme_minimal() +
+    theme(legend.position = "none")
+) + (
+  temp %>% 
+    ggplot() +
+    geom_line(aes(value, value_perc, color=key), size=1) +
+    geom_vline(xintercept = 0) + 
+    scale_color_brewer(palette="Set2") +
+    xlab("minutes") +
+    ylab("cumulative") + 
+    scale_x_continuous(limits = c(min_min, max_min)) +
+    theme_minimal() +
+    theme(legend.position = "bottom")
+) + plot_layout(ncol = 1, heights = c(1, 1))
+
+temp <- raw_data %>%
+  filter(train_number==9747) %>%
+  filter(!is.na(seg_delay), abs(seg_delay)<100, abs(fin_delay)<180) %>% 
+  mutate(perc_inc_delay = percent_rank(inc_delay),
+         perc_seg_delay = percent_rank(seg_delay),
+         perc_fin_delay = percent_rank(fin_delay)) %>% 
+  select(inc_delay, perc_inc_delay, seg_delay, perc_seg_delay, fin_delay, perc_fin_delay) %>% 
+  gather("key_perc", "value_perc", -c("inc_delay", "seg_delay", "fin_delay")) %>% 
+  gather("key", "value", -c("key_perc", "value_perc")) %>% 
+  mutate(key_perc = str_replace_all(key_perc, "^perc_", "")) %>% 
+  filter(key_perc == key) %>% 
+  mutate(key = fct_relevel(key, "inc_delay", "seg_delay", "fin_delay")) 
+
+max_min <- 60
+min_min <- -10
+
+(
+  temp %>% 
+    ggplot() +
+    geom_density(aes(value, fill=key, color=key), alpha=.4) +
+    geom_vline(xintercept = 0) + 
+    scale_color_brewer(palette="Set2") +
+    scale_fill_brewer(palette="Set2") +
+    ggtitle("Train 9747 (TO > VE, late afternoon) punctuality") +
+    xlab("minutes") +
+    scale_x_continuous(limits = c(min_min, max_min)) +
+    theme_minimal() +
+    theme(legend.position = "none")
+) + (
+  temp %>% 
+    ggplot() +
+    geom_boxplot(aes(y=value, x=key, color=key)) +
+    geom_hline(yintercept = 0) +
+    scale_color_brewer(palette="Set2") +
+    scale_fill_brewer(palette="Set2") +
+    ylab("minutes") +
+    xlab("") +
+    scale_y_continuous(limits = c(min_min, max_min)) +
+    coord_flip() +
+    theme_minimal() +
+    theme(legend.position = "none")
+) + (
+  temp %>% 
+    ggplot() +
+    geom_line(aes(value, value_perc, color=key), size=1) +
+    geom_vline(xintercept = 0) + 
+    scale_color_brewer(palette="Set2") +
+    xlab("minutes") +
+    ylab("cumulative") + 
+    scale_x_continuous(limits = c(min_min, max_min)) +
+    theme_minimal() +
+    theme(legend.position = "bottom")
+) + plot_layout(ncol = 1, heights = c(1, 1))
+
+
+temp <- raw_data %>%
+  filter(train_number==9747) %>%
+  filter(!is.na(seg_delay), abs(seg_delay)<100, abs(fin_delay)<180) %>% 
+  left_join(., stations %>% select(id, nomeBreve), by=c("from_id"="id")) %>% 
+  rename("from"="nomeBreve") %>% 
+  left_join(., stations %>% select(id, nomeBreve), by=c("to_id"="id")) %>% 
+  rename("to"="nomeBreve") %>% 
+  mutate(segment = ifelse(from==to, from, paste0(from, " > ", to)),
+         segment = fct_reorder(segment, -step)) %>% 
+  select(trip_date, step, segment, fin_delay)
+
+temp %>% 
+  ggplot(aes(x=segment, y=fin_delay, group=trip_date)) +
+  geom_hline(yintercept = 0, col="red") +
+  geom_line(alpha=.4) +
+  stat_summary(aes(x=segment, y=fin_delay, group=segment), fun.y = "median", colour = "red", geom = "point") +
+  ylab("delay") + 
+  ggtitle("Train 9747 (To > Ve)") +
+  coord_flip() +
   theme_minimal()
 
+library(purrr)
+p <- c(0.025, 0.05, 0.25, 0.50, 0.75, 0.95, 0.975)
+p_names <- map_chr(p, ~paste0("perc_", .x*1000))
+p_funs <- map(p, ~partial(quantile, probs = .x, na.rm = TRUE)) %>% 
+  set_names(nm = p_names)
 
-raw_data %>% filter(!is.na(seg_delay), abs(seg_delay)<60) %>% pull(seg_delay) %>% median()
-raw_data %>% filter(!is.na(fin_delay), abs(fin_delay)<120) %>% pull(fin_delay) %>% median()
+(
+  raw_data %>%
+    filter(train_number==9747) %>%
+    filter(!is.na(seg_delay), abs(seg_delay)<100, abs(fin_delay)<180) %>% 
+    left_join(., stations %>% select(id, nomeBreve), by=c("from_id"="id")) %>% 
+    rename("from"="nomeBreve") %>% 
+    left_join(., stations %>% select(id, nomeBreve), by=c("to_id"="id")) %>% 
+    rename("to"="nomeBreve") %>% 
+    mutate(segment = ifelse(from==to, from, paste0(from, " > ", to)),
+           segment = strtrim(segment, 30),
+           segment = fct_reorder(segment, -step)) %>% 
+    select(trip_date, step, segment, fin_delay) %>% 
+    group_by(segment) %>% 
+    summarize_at(vars(fin_delay), funs(!!!p_funs)) %>%
+    ggplot(aes(x=segment, group=1)) +
+    # geom_line(data = temp, aes(x=segment, y=fin_delay, group=trip_date), alpha=.2) +
+    geom_ribbon(aes(ymin=`perc_25`, ymax=`perc_975`), alpha=.15) + 
+    geom_ribbon(aes(ymin=`perc_50`, ymax=`perc_950`), alpha=.15) +
+    geom_ribbon(aes(ymin=`perc_250`, ymax=`perc_750`), alpha=.15) +
+    geom_line(aes(y=perc_500)) +
+    ylab("delay (min)") +
+    scale_y_continuous(limits = c(-10, 60)) +
+    coord_flip() +
+    theme_minimal()
+) + (
+  raw_data %>%
+    filter(train_number==10918) %>%
+    filter(!is.na(seg_delay), abs(seg_delay)<100, abs(fin_delay)<180) %>% 
+    left_join(., stations %>% select(id, nomeBreve), by=c("from_id"="id")) %>% 
+    rename("from"="nomeBreve") %>% 
+    left_join(., stations %>% select(id, nomeBreve), by=c("to_id"="id")) %>% 
+    rename("to"="nomeBreve") %>% 
+    mutate(segment = ifelse(from==to, from, paste0(from, " > ", to)),
+           segment = strtrim(segment, 30),
+           segment = fct_reorder(segment, -step)) %>% 
+    select(trip_date, step, segment, fin_delay) %>% 
+    group_by(segment) %>% 
+    summarize_at(vars(fin_delay), funs(!!!p_funs)) %>%
+    ggplot(aes(x=segment, group=1)) +
+    # geom_line(data = temp, aes(x=segment, y=fin_delay, group=trip_date), alpha=.2) +
+    geom_ribbon(aes(ymin=`perc_25`, ymax=`perc_975`), alpha=.15) + 
+    geom_ribbon(aes(ymin=`perc_50`, ymax=`perc_950`), alpha=.15) +
+    geom_ribbon(aes(ymin=`perc_250`, ymax=`perc_750`), alpha=.15) +
+    geom_line(aes(y=perc_500)) +
+    ylab("delay (min)") +
+    scale_y_continuous(limits = c(-10, 60)) +
+    coord_flip() +
+    theme_minimal()
+)
 
 
-raw_data %>% 
-  filter(!is.na(fin_delay), abs(fin_delay)<120) %>%
-  pull(fin_delay) %>% 
-  quantile(., seq(0,1,0.001)) %>% 
-  plot(seq(0,1,0.001), ., type="l")
 
 #
-
-
-
-
-
-
-
 
 
 
